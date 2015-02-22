36b87cebc6622c53c8dc9b033ec10eeb2e1ce738
RyoriMisawa <ryori@RyoriMisawa-no-MacBook-Air.local>
2015-02-23 03:14:23 +0900

    test commit

--- Classes/Bird.h
+++ Classes/Bird.h
@@ -11,7 +11,7 @@ public:
     void Fall( );
     void Fly( ) { CocosDenshion::SimpleAudioEngine::getInstance( )->playEffect( "Sounds/Wing.mp3" ); isFalling = false; };
     void StopFlying( ) { isFalling = true; }
-    
+    //
 private:
     cocos2d::Size visibleSize;
     cocos2d::Vec2 origin;
